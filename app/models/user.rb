f0b9863e2bafcdea02f18b5ada45968a8334a129
marokis <ligotjogo0502@gmail.com>
2021-09-08 11:25:21 +0900

    バリデーションの実装#2編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,15 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
-  with_option presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :last_name
     validates :first_name
+  end
 
-  with_option presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
     validates :last_name_kana
     validates :first_name_kana
+  end
 
   validates :dob, presence: true
 
