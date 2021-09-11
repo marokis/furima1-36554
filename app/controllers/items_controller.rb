89f3f73759cfdf27a2ff60532bc0c5408245e8f4
marokis <ligotjogo0502@gmail.com>
2021-09-11 11:32:26 +0900

    create アクションの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,13 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name :price :user :detail :category_id :status_id :charge_id :datetime_id :prefecture_id)
+    
 end
