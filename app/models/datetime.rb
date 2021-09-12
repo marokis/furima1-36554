43b6a0f61efb44a8b653dc13d7ae2ac36d8a8180
marokis <ligotjogo0502@gmail.com>
2021-09-12 16:18:58 +0900

    データの編集

--- app/models/datetime.rb
+++ app/models/datetime.rb
@@ -2,7 +2,8 @@ class Datetime < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: '1~3日で発送' },
-    { id: 3, name: '4~7日で発送' }
+    { id: 3, name: '2~3日で発送' },
+    { id: 4, name: '4~7日で発送' }
   ]
 
   include ActiveHash::Associations
