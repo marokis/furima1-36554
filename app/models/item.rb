61038e39de8a8c6239b6c6b5e86aec65d3dee415
marokis <ligotjogo0502@gmail.com>
2021-09-12 16:18:07 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one :order
   has_one_attached :image
@@ -7,4 +9,13 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
   end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :day_id
+  end
+  
 end
