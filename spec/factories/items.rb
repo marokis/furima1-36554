f06e601230cac7adaf85e975d9a6306afc005c4c
marokis <ligotjogo0502@gmail.com>
2021-09-12 21:38:45 +0900

    テストコードの記入

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
-=begin  factory :item do
+  factory :item do
     name                  { Faker::Name.initials(number: 5)}
     detail                { Faker::Lorem.sentence }
     category_id           {2}
     status_id             {2}
     charge_id             {2}
+    prefecture_id         {2}
     datetime_id           {2}
     price                 {1000}
     association           :user
@@ -12,7 +13,5 @@ FactoryBot.define do
     after(:build) do |item|
       item.image.attach(io: File.open('app/assets/images/item-sample.png'), filename: 'item-sample.png')
     end
-
-  end 
-=end
+  end
 end
