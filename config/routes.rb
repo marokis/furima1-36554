e555c31eca365b83ef004d6c019ef2144995b5c6
marokis <ligotjogo0502@gmail.com>
2021-09-13 12:55:07 +0900

    コードレビュー編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resource :items, only: [:index, :new, :create, :edit]
+  resource :items, only: [:index, :new, :create]
 
 end
