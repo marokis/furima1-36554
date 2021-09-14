5ec62746295658803313d1284e59bb4fe023e372
marokis <ligotjogo0502@gmail.com>
2021-09-14 20:43:14 +0900

    アクションの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
   
   def update
-    if @item.update(user_params)
+    if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
