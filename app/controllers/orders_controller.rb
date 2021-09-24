f4ae80c99c84e37cde6a2736afb10ff09fa24206
marokis <ligotjogo0502@gmail.com>
2021-09-24 14:58:58 +0900

    コミット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,12 +24,12 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :place, :building, :phone_number)
            .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
       amount: @item.price,  
       card: order_params[:token],    
