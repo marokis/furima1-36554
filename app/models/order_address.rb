3c3f4a3428b8944354f354437c50f134bc63ba9a
marokis <ligotjogo0502@gmail.com>
2021-09-26 22:21:10 +0900

    Update order_address.rb

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,12 +1,12 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :place, :building, :phone_number, :user_id, :item_id, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :block, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
-    validates :place
+    validates :block
     validates :user_id
     validates :item_id
     validates :token
@@ -18,7 +18,7 @@ class OrderAddress
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
     Address.create(
-      postal_code: postal_code, prefecture_id: prefecture_id, city: city, place: place, 
+      postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, 
       building: building, phone_number: phone_number, order_id: order.id
     )
   end
